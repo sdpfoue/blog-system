aa1e9d0c2ac2fe2101098b8880cd471f17dd11cc
sdpfoue <stephen.j.tong@gmail.com>
2012-09-19 21:22:12 +0800

    add web

---
+++ web/index.php
@@ -0,0 +1,11 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", "On");
+date_default_timezone_set('Asia/Shanghai');
+
+require_once(dirname(__FILE__).'/../etc/config/ProjectConfiguration.class.php');
+
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
+sfContext::createInstance($configuration)->dispatch();
+
+?>
