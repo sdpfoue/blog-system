6a7221054c579f385621a801ffb6b098d23d18bc
Stephen <stephen.j.tong@gmail.com>
2013-12-28 20:54:52 +0800

    update about

--- apps/frontend/modules/index/templates/aboutSuccess.php
+++ apps/frontend/modules/index/templates/aboutSuccess.php
@@ -2,17 +2,19 @@
   About - 
 <?php end_slot();?>
 
-<h2>About</h2>
 且论吃喝，兼谈技术，偶谑风月，不言国事<br/>
 <br/>
-GTalk / Email:<br/>
-执行以下代码可得 <br/>
-dc -e724720727228167274512831516015536398467282190527270721907978P
+GTalk: <br/>
+<img src="http://i.minus.com/ibSzffRmk36Ss.gif"><br/><br/>
+Email: <br/>
+<img src="http://safemail.justlikeed.net/e/ba54921541d3e83ebcfd1497efe72065.png" border="1" >
 <p>
+    <a href="http://www.linkedin.com/profile/view?id=59188482&trk=nav_responsive_tab_profile" target="_blank">LinkedIn</a><br/>
     <a href="http://www.douban.com/people/tjs/" target="_blank">豆瓣</a><br/>
     <a href="https://plus.google.com/109662947028610532436/" target="_blank">Google+</a><br/>
     <a href="http://weibo.com/tongjishi" target="_blank">新浪微博</a><br/>
     <a href="https://github.com/sdpfoue" target="_blank">Github</a><br/>
+    <a href="https://gist.github.com/sdpfoue" target="_blank">Gist</a><br/>
 </p>
 <p>
 过去时：<a href="http://360.cn" target="_blank">奇虎360</a>, <a href="http://www.adobe.com" target="_blank">Adobe Systems</a>
